a990cb9b743ff4ef21bcd306dc5b0449675bfa5d
ys-grouse <malsawma7777@gmail.com>
2024-03-27 11:08:25 +0530

    chore[init]: fly

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,6 +25,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
+            ->spa()
             ->default()
             ->id('admin')
             ->path('')
